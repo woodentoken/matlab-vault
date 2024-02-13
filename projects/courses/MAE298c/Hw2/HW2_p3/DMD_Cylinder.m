a4d4b8ef25b21274e244a0ba08c6ad8f8bb15ea3
Kaleb Bordner <kaleb.bordner@gmail.com>
2024-02-13 09:52:50 -0800

    add

--- projects/courses/MAE298c/Hw2/HW2_p3/DMD_Cylinder.m
+++ projects/courses/MAE298c/Hw2/HW2_p3/DMD_Cylinder.m
@@ -2,7 +2,7 @@ clearvars; close all; clc
 load('CylinderFlow.mat');
 
 % CylinderFlow contains flow fields reshaped into column vectors
-X = CylinderFlow(:,1:end-1);  % data matrix X
+X = CylinderFlow(:,1:end-1); % data matrix X
 Xp = CylinderFlow(:,2:end);  % data matrix X prime
 
 %% part a:  visualizing the flow field
@@ -12,7 +12,7 @@ subplot(3, 3, 1)
 for index = 1:length(time_snaps)
     axisi = subplot(3, 3, index);
     plotCylinder(axisi, real(reshape(X(:,time_snaps(index)),199,449)));
-    title(['t = ',num2str(time_snaps(index), '%d')])
+    title(['t = ', num2str(time_snaps(index), '%d')])
 end
 
 %% part b: 
@@ -22,13 +22,13 @@ end
 %% part c: 
 
 r= 5;
-[Phi_5, Lambda_5, b_5] = DMD(X,Xp,r);
+[Phi_5, Lambda_5, b_5] = DMD(X, Xp, r);
 
 r= 20;
-[Phi_20, Lambda_20, b_20] = DMD(X,Xp,r);
+[Phi_20, Lambda_20, b_20] = DMD(X, Xp, r);
 
 
 %% part d:
 
-%x_10 = ;
+% x_10 = ;
 
